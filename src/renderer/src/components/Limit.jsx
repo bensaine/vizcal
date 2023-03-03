c31ac7b10e2be445681e37ddaa2e0c03a30b3cd4
Ben <bensaine09@gmail.com>
2023-03-03 11:02:14 -0500

    add: Limit implementation

--- src/renderer/src/components/Limit.jsx
+++ src/renderer/src/components/Limit.jsx
@@ -8,8 +8,9 @@ import { StaticMath } from './StaticMath'
 
 export const Limit = () => {
 	const [fx, setFx] = useState('')
-	const [x, setX] = useState([0, 10])
-	const [direction, setDirection] = useState('left')
+	const [x, setX] = useState(0)
+	const [epsilon, setEpsilon] = useState(0.1)
+	const [delta, setDelta] = useState(0.1)
 
 	const renderOptions = () => {
 		return (
@@ -23,25 +24,130 @@ export const Limit = () => {
 						setFx(input.latex())
 					}}
 				/>
-				<Slider id="x" label="x" value={x} onChange={setX} min={-20} max={10} step={0.1} />
-				<Dropdown
-					id="direction"
-					label="Direction"
-					options={['left', 'right']}
-					onChange={setDirection}
+				<Slider
+					id="x"
+					label="x point"
+					value={x}
+					onChange={setX}
+					min={-20}
+					max={10}
+					step={0.1}
+					disabled={fx == ''}
+				/>
+				<Slider
+					id="epsilon"
+					label="Epsilon"
+					value={epsilon}
+					onChange={setEpsilon}
+					min={0}
+					max={20}
+					step={0.0001}
+					disabled={fx == ''}
+				/>
+				<Slider
+					id="delta"
+					label="Delta"
+					value={delta}
+					onChange={setDelta}
+					min={0}
+					max={20}
+					step={0.0001}
+					disabled={fx == ''}
 				/>
 			</>
 		)
 	}
 
 	const renderGraph = () => {
-		return <Expression id="function" latex={fx} />
+		return (
+			<>
+				<Expression id="function" latex={'f\\left(x\\right)=' + fx} color="#fff" />
+				{fx != '' && (
+					<>
+						<Expression id="x" latex={'x_{point}=' + x} />
+						<Expression id="epsilon" latex={'e_{psilon}=' + epsilon} />
+						<Expression id="delta" latex={'d_{elta}=' + delta} />
+						<Expression id="xLine" latex={'x=x_{point}'} color="#37a" />
+						<Expression
+							id="pointOnFunction"
+							latex={'\\left(x_{point},f\\left(x_{point}\\right)\\right)'}
+							dragMode="NONE"
+						/>
+						<Expression id="yLine" latex={'y=f\\left(x_{point}\\right)'} />
+						<Expression
+							id="xAxisPoint"
+							latex={'\\left(x_{point},0\\right)'}
+							color="#37a"
+						/>
+						<Expression
+							id="yDown"
+							latex={'y_{down}=f\\left(x_{point}\\right)-e_{psilon}'}
+						/>
+						<Expression
+							id="yUp"
+							latex={'y_{up}=f\\left(x_{point}\\right)+e_{psilon}'}
+						/>
+						<Expression
+							id="yDownLine"
+							latex={'y=y_{down}'}
+							lineStyle="DASHED"
+							lineWidth={1.5}
+							lineOpacity={0.6}
+							color="#fff"
+						/>
+						<Expression
+							id="yUpLine"
+							latex={'y=y_{up}'}
+							lineStyle="DASHED"
+							lineWidth={1.5}
+							lineOpacity={0.6}
+							color="#fff"
+						/>
+						<Expression id="xLeft" latex={'x_{left}=x_{point}-d_{elta}'} />
+						<Expression id="xRight" latex={'x_{right}=x_{point}+d_{elta}'} />
+						<Expression
+							id="xLeftLine"
+							latex={'x=x_{left}'}
+							lineStyle="DASHED"
+							lineWidth={1.5}
+							lineOpacity={0.6}
+							color="#fff"
+						/>
+						<Expression
+							id="xRightLine"
+							latex={'x=x_{right}'}
+							lineStyle="DASHED"
+							lineWidth={1}
+							lineOpacity={0.6}
+							color="#fff"
+						/>
+						<Expression id="yUpDelta" latex={'y_{upDelta}=f\\left(x_{right}\\right)'} />
+						<Expression
+							id="yDownDelta"
+							latex={'y_{downDelta}=f\\left(x_{left}\\right)'}
+						/>
+						<Expression
+							id="yUpDeltaLine"
+							latex={'y=y_{upDelta}'}
+							lineWidth={1.5}
+							color="#37a"
+						/>
+						<Expression
+							id="yDownDeltaLine"
+							latex={'y=y_{downDelta}'}
+							lineWidth={1.5}
+							color="#37a"
+						/>
+					</>
+				)}
+			</>
+		)
 	}
 
 	const renderHelp = () => {
 		return (
 			<>
-				<p>HO SAY YUNG FROM SQUID GAME?</p>
+				<p>todo</p>
 				<StaticMath>
 					{'lim_{h\\to0}\\frac{f\\left(x+h\\right)-f\\left(x\\right)}{h}'}
 				</StaticMath>
