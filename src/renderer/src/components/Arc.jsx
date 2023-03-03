f82357efe8cbc3675e5ea4d61b35cabd940928b5
Steev3n <91221946+Steev3n@users.noreply.github.com>
2023-03-03 01:09:12 -0500

    Arc Length help done

--- src/renderer/src/components/Arc.jsx
+++ src/renderer/src/components/Arc.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import { Experiment } from './Experiment/Experiment'
 import { MathInput } from './Controls/MathInput'
 import { Slider } from './Controls/Slider/Slider'
+import { StaticMath } from './StaticMath'
+import ArcImg1 from '../assets/images/arclength1.png'
+import ArcImg2 from '../assets/images/arclength2.png'
 
 export const Arc = () => {
 	const [fx, setFx] = useState('')
@@ -85,6 +88,42 @@ export const Arc = () => {
 			</>
 		)
 	}
+	const renderHelp = () => {
+		return (
+			<>
+				<h3>What is an arc length?</h3>
+				<p>
+					The arc length is an application of integration that lets us find the length of a function within a certain range.
+					The method by which this is achieved it very similar to the approximation of the area under a curve
+					where Riemann sums where exhaustively used.
+				</p>
+				
+				<h3>What is the formula?</h3>
+				<p>
+					We start by dividing our range into n sections with equal width
+					<StaticMath>
+						{' \\Delta x=\\frac{b-a}{n}'}
+					</StaticMath>
+					and define <StaticMath>{'x_{i}*=a+i\\Delta x'}</StaticMath>
+					where <StaticMath>{'0 < i \\leq n'}</StaticMath>. Then, we join all points for <StaticMath>{'f(x_{i}) \\space and \\space f(x_{i-1})'}</StaticMath> 
+					to create line segments that approximate the length of the curve.
+				</p>
+				<img src={ArcImg1}/>
+				<p>
+					To get the numerical approximation, we repeatedly use the distance formula
+					<StaticMath>{"\\sqrt{(\\Delta x)^2 + (f'(x_{i}*)\\Delta x)^2} = \\sqrt{1 + [f'(x_{i}*)]^2}\\Delta x"}</StaticMath>.
+					Lastly, we take <StaticMath>{"\\lim_{n\\to\\infty}\\sum_{i=1}^{n}{{\\sqrt{1+[f'(x_{i}*)]^2}}\\Delta x}"}</StaticMath> to go over all line segments
+					as well as refining the approximation infinitely well.
+				</p>
+				<img src={ArcImg2}/>	
+				<p>
+					This gives us the formula for the arc length:
+				</p>
+				
+				<StaticMath>{"L = \\int_{a}^{b}{\\sqrt{1+[f'(x)]^2}}dx"}</StaticMath>
+			</>
+		)
+	}
 
-	return <Experiment optionsSlot={renderOptions} graphSlot={renderGraph} />
+	return <Experiment optionsSlot={renderOptions} graphSlot={renderGraph} helpSlot={renderHelp}/>
 }
