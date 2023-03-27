c623e06fb073c17962289a838dad648635f3e669
wawastyle911 <104150533+wawastyle911@users.noreply.github.com>
2023-03-27 15:45:28 -0400

    add: collapsable options

--- src/renderer/src/components/ExperimentBase/ExperimentBase.jsx
+++ src/renderer/src/components/ExperimentBase/ExperimentBase.jsx
@@ -3,22 +3,29 @@ import { useEffect, useState } from 'react'
 import '../../../jquery.js'
 import '../../../desmos.js'
 import { Dialog } from '../Dialog/Dialog'
-import { HelpCircle } from 'react-feather'
+import { HelpCircle, ArrowLeftCircle, ArrowRightCircle } from 'react-feather'
 import '../../assets/desmos.css'
 import { GraphingCalculator } from 'desmos-react'
 
 export const ExperimentBase = ({ optionsSlot, graphSlot, helpSlot = () => {} }) => {
 	const [helpOpen, setHelpOpen] = useState(false)
+	const [isCollapsed, setIsCollapsed] = useState(false)
 
 	return (
 		<div className={styles.container}>
-			<div className={styles.options}>
+			<div className={styles.options + (isCollapsed ? ' ' + styles.collapsed : '')}>
 				<span className={styles.helpButton} onClick={() => setHelpOpen(true)}>
 					<HelpCircle />
 				</span>
 				{optionsSlot()}
 			</div>
 			<div className={styles.output}>
+				<span
+					className={styles.collapseButton}
+					onClick={() => setIsCollapsed(!isCollapsed)}
+				>
+					{isCollapsed ? <ArrowRightCircle /> : <ArrowLeftCircle />}
+				</span>
 				<GraphingCalculator
 					attributes={{ className: styles.calculator }}
 					fontSize={12}
