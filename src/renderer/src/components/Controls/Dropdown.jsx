3ddfaf7f728a35254bd1fc965728c37e10c46b44
wawastyle911 <104150533+wawastyle911@users.noreply.github.com>
2023-03-27 23:24:00 -0400

    fix(Dropdown): initial value

--- src/renderer/src/components/Controls/Dropdown.jsx
+++ src/renderer/src/components/Controls/Dropdown.jsx
@@ -1,9 +1,9 @@
 import { ControlContainer } from './ControlContainer/ControlContainer'
 
-export const Dropdown = ({ id, label, options, selected, onChange, disabled }) => {
+export const Dropdown = ({ id, label, options, value, onChange, disabled }) => {
 	return (
 		<ControlContainer id={id} label={label} disabled={disabled}>
-			<select value={selected} onChange={(e) => onChange(e.target.value)} disabled={disabled}>
+			<select value={value} onChange={(e) => onChange(e.target.value)} disabled={disabled}>
 				{options.map((option) => (
 					<option key={option} value={option}>
 						{option}
