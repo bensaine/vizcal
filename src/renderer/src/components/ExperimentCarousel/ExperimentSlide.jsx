f627175fa9da260a455e0ee97ed16bd288b638f4
Qui4 <96349796+Qui4@users.noreply.github.com>
2023-04-12 00:00:58 -0400

    Added icons for Epsilon Delta, Slope, Reimann sum, and Arc Length
    The icons are now displayed in the carousel and a ccs rule has been implemented to resize them.

--- src/renderer/src/components/ExperimentCarousel/ExperimentSlide.jsx
+++ src/renderer/src/components/ExperimentCarousel/ExperimentSlide.jsx
@@ -1,9 +1,11 @@
 import styles from './ExperimentCarousel.module.scss'
 
-export const ExperimentSlide = ({ id, displayName, category }) => {
+export const ExperimentSlide = ({ id, displayName, category, image }) => {
 	return (
 		<div className={styles.experiment}>
+			<img className={styles.icon} src={image} alt=""></img>
 			<span className={styles.title}>{displayName}</span>
+
 			{/* <span className={styles.subtitle}>{category}</span> */}
 		</div>
 	)
--- src/renderer/src/data/experiments.js
+++ src/renderer/src/data/experiments.js
@@ -8,24 +8,28 @@ export const experiments = [
 		type: 'epsilonDelta',
 		displayName: 'Epsilon-Delta',
 		category: 'Limits',
+		image: '/src/assets/images/icons/eps.svg',
 		component: Limit
 	},
 	{
 		type: 'slope',
 		displayName: 'Slope',
 		category: 'Derivatives',
+		image: '/src/assets/images/icons/slope.svg',
 		component: Derivative
 	},
 	{
 		type: 'riemannSum',
 		displayName: 'Riemann Sum',
 		category: 'Area Under Curve',
+		image: '/src/assets/images/icons/reimann.svg',
 		component: Riemann
 	},
 	{
 		type: 'arcLength',
 		displayName: 'Arc Length',
 		category: 'Arc Length',
+		image: '/src/assets/images/icons/arc.svg',
 		component: Arc
 	}
 ]
