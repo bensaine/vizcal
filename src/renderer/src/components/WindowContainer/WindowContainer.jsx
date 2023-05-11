40bd00ab46f84c6a4d185074e1e78d55c4a4d74e
Ben <bensaine09@gmail.com>
2023-05-11 17:17:09 -0400

    enhance: use semantic html tags

--- src/renderer/src/components/WindowContainer/WindowContainer.jsx
+++ src/renderer/src/components/WindowContainer/WindowContainer.jsx
@@ -1,5 +1,5 @@
 import styles from './WindowContainer.module.scss'
 
 export const WindowContainer = ({ children }) => {
-	return <div className={styles.windowContainer}>{children}</div>
+	return <main className={styles.windowContainer}>{children}</main>
 }
