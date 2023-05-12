d8b169b0f0a7226823a3007f62e9dc99a46343db
wawastyle911 <104150533+wawastyle911@users.noreply.github.com>
2023-05-12 17:01:54 -0400

    add(DropDown):documentation

--- src/renderer/src/components/Controls/Dropdown.jsx
+++ src/renderer/src/components/Controls/Dropdown.jsx
@@ -1,5 +1,19 @@
 import { ControlContainer } from './ControlContainer/ControlContainer'
 
+/**
+ * Dropdown component. A dropdown control that allows user to choose between options that change the dropdown's value.
+ *
+ * @author Benjamin Saine
+ * @component
+ * @param {Object} props - The component props.
+ * @param {string} props.id - The unique identifier of the dropdown element.
+ * @param {string} props.label - The label for the dropdown element.
+ * @param {Array} props.options - The options available in the dropdown.
+ * @param {string} props.value - The currently selected value in the dropdown.
+ * @param {function} props.onChange - The callback function called when the selected value changes.
+ * @param {boolean} props.disabled - Specifies if the dropdown should be disabled.
+ * @returns {ReactElement} The rendered Dropdown component.
+ */
 export const Dropdown = ({ id, label, options, value, onChange, disabled }) => {
 	return (
 		<ControlContainer id={id} label={label} disabled={disabled}>
