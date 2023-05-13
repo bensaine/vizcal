c13402ff27f2f0d254e3cf4a7c885117d23493f8
Qui4 <96349796+Qui4@users.noreply.github.com>
2023-05-12 20:33:08 -0400

    add(Dialog): JSDoc

--- src/renderer/src/components/Dialog/Dialog.jsx
+++ src/renderer/src/components/Dialog/Dialog.jsx
@@ -1,6 +1,23 @@
 import { X } from 'react-feather'
 import styles from './Dialog.module.scss'
-
+/**
+ * A dialog component that can be opened or closed by changing the value of the `open` prop.
+ * It displays a title, content and a close button. It also includes a backdrop that can be clicked
+ * to close the dialog.
+ *
+ * @param {Object} props - The props object.
+ * @param {string} props.title - The title of the dialog.
+ * @param {JSX.Element} props.children - The content to be displayed in the dialog.
+ * @param {boolean} props.open - Whether the dialog is open or closed.
+ * @param {function} props.onClose - The function to be called when the dialog is closed.
+ *
+ * @example
+ * <Dialog open={dialogOpen} title={'Dialog'} onClose={() => setDialogOpen(false)}>
+ * <p>This is the content of my dialog.</p>
+ * </Dialog>
+ *
+ * @returns {JSX.Element} The dialog component.
+ */
 export const Dialog = ({ title, children, open, onClose }) => {
 	return (
 		<div className={styles.container} style={{ display: open ? 'flex' : 'none' }}>
